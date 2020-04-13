90979a3f48ffe82812400d6c5b3f9e2a2e8dd35c
saanuregh <saanu09xs@gmail.com>
2020-04-13 22:03:05 +0530

    updated deps

--- src/components/map.js
+++ src/components/map.js
@@ -7,6 +7,7 @@ import { useWindowWidth } from "@react-hook/window-size/throttled";
 
 function Map({ districts, total, maxConfirmed }) {
   const [district, setDistrict] = useState({});
+  const [renderData, setRenderData] = useState(null);
   const [curLang, setCurLang] = useState([]);
   const [mapHeight, setMapHeight] = useState(0);
   const width = useWindowWidth(450, { fps: 30, leading: true, wait: 0 });
@@ -20,118 +21,131 @@ function Map({ districts, total, maxConfirmed }) {
   }, [total]);
 
   useEffect(() => {
+    if (map.current && renderData) {
+      d3.selectAll("svg > *").remove();
+      const svg = d3.select(map.current);
+      const topology = topojson.feature(
+        renderData,
+        renderData.objects.kerala_district
+      );
+      const projection = d3.geoMercator();
+      projection.fitHeight(+svg.attr("height"), topology);
+      const path = d3.geoPath(projection);
+      const maxInterpolation = 0.8;
+      const color = d3
+        .scaleSequential(d3.interpolateReds)
+        .domain([0, maxConfirmed / maxInterpolation]);
+      svg
+        .append("g")
+        .attr("class", "kerala")
+        .selectAll("path")
+        .data(topology.features)
+        .enter()
+        .append("path")
+        .attr("fill", function (d) {
+          const n = districts[d.properties.district].corona_positive;
+          return d3.interpolateReds((maxInterpolation * n) / maxConfirmed);
+        })
+        .attr("d", path)
+        .attr("pointer-events", "all")
+        .on("mouseenter", (d) => {
+          if (districts[d.properties.district]) {
+            const current = d.properties.district;
+            setDistrict({
+              name: current,
+              ...districts[current],
+            });
+          }
+          const target = d3.event.target;
+          d3.select(target.parentNode.appendChild(target))
+            .attr("stroke", "#ff073a")
+            .attr("stroke-width", 2);
+        })
+        .on("mouseleave", (d) => {
+          resetDistrict();
+          const target = d3.event.target;
+          d3.select(target).attr("stroke", "None");
+        })
+        .style("cursor", "pointer")
+        .append("title")
+        .text(function (d) {
+          return (
+            parseFloat(
+              100 *
+                (parseInt(districts[d.properties.district].corona_positive) /
+                  total.corona_positive)
+            ).toFixed(2) +
+            "% from " +
+            d.properties.district
+          );
+        });
+      svg
+        .append("path")
+        .attr("stroke", "#ff073a20")
+        .attr("fill", "none")
+        .attr("stroke-width", 2)
+        .attr(
+          "d",
+          path(topojson.mesh(renderData, renderData.objects.kerala_district))
+        );
+      if (width < 500) {
+        svg
+          .append("g")
+          .attr("class", "legend")
+          .attr("transform", "translate(5, 325)");
+      } else {
+        svg
+          .append("g")
+          .attr("class", "legend")
+          .attr("transform", "translate(5, 365)");
+      }
+      const numCells = 6;
+      const delta = Math.floor(
+        (maxConfirmed < numCells ? numCells : maxConfirmed) / (numCells - 1)
+      );
+      const cells = Array.from(Array(numCells).keys()).map((i) => i * delta);
+      function label({ i, genLength, generatedLabels }) {
+        if (i === genLength - 1) {
+          const n = Math.floor(generatedLabels[i]);
+          return `${n}+`;
+        } else {
+          const n1 = 1 + Math.floor(generatedLabels[i]);
+          const n2 = Math.floor(generatedLabels[i + 1]);
+          return `${n1} - ${n2}`;
+        }
+      }
+      const legend = legendColor()
+        .shapeWidth(30)
+        .cells(cells)
+        .titleWidth(3)
+        .labels(label)
+        .title("Confirmed Cases")
+        .orient("vertical")
+        .scale(color);
+      svg.select(".legend").call(legend);
+    }
+  }, [
+    districts,
+    maxConfirmed,
+    renderData,
+    resetDistrict,
+    total.corona_positive,
+    width,
+  ]);
+
+  useEffect(() => {
     if (
       Object.keys(districts).length > 0 &&
       map.current &&
       total.corona_positive
     ) {
       (async () => {
-        resetDistrict();
-        d3.selectAll("svg > *").remove();
         const kerala = await d3.json("/kerala.json");
-        const svg = d3.select(map.current);
-        const topology = topojson.feature(
-          kerala,
-          kerala.objects.kerala_district
-        );
-        const projection = d3.geoMercator();
-        projection.fitHeight(+svg.attr("height"), topology);
-        const path = d3.geoPath(projection);
-        const maxInterpolation = 0.8;
-        const color = d3
-          .scaleSequential(d3.interpolateReds)
-          .domain([0, maxConfirmed / maxInterpolation]);
-        svg
-          .append("g")
-          .attr("class", "kerala")
-          .selectAll("path")
-          .data(topology.features)
-          .enter()
-          .append("path")
-          .attr("fill", function (d) {
-            const n = districts[d.properties.district].corona_positive;
-            return d3.interpolateReds((maxInterpolation * n) / maxConfirmed);
-          })
-          .attr("d", path)
-          .attr("pointer-events", "all")
-          .on("mouseenter", (d) => {
-            if (districts[d.properties.district]) {
-              const current = d.properties.district;
-              setDistrict({
-                name: current,
-                ...districts[current],
-              });
-            }
-            const target = d3.event.target;
-            d3.select(target.parentNode.appendChild(target))
-              .attr("stroke", "#ff073a")
-              .attr("stroke-width", 2);
-          })
-          .on("mouseleave", (d) => {
-            resetDistrict();
-            const target = d3.event.target;
-            d3.select(target).attr("stroke", "None");
-          })
-          .style("cursor", "pointer")
-          .append("title")
-          .text(function (d) {
-            return (
-              parseFloat(
-                100 *
-                  (parseInt(districts[d.properties.district].corona_positive) /
-                    total.corona_positive)
-              ).toFixed(2) +
-              "% from " +
-              d.properties.district
-            );
-          });
-        svg
-          .append("path")
-          .attr("stroke", "#ff073a20")
-          .attr("fill", "none")
-          .attr("stroke-width", 2)
-          .attr(
-            "d",
-            path(topojson.mesh(kerala, kerala.objects.kerala_district))
-          );
-        if (width < 500) {
-          svg
-            .append("g")
-            .attr("class", "legend")
-            .attr("transform", "translate(5, 325)");
-        } else {
-          svg
-            .append("g")
-            .attr("class", "legend")
-            .attr("transform", "translate(5, 365)");
-        }
-        const numCells = 6;
-        const delta = Math.floor(
-          (maxConfirmed < numCells ? numCells : maxConfirmed) / (numCells - 1)
-        );
-        const cells = Array.from(Array(numCells).keys()).map((i) => i * delta);
-        function label({ i, genLength, generatedLabels }) {
-          if (i === genLength - 1) {
-            const n = Math.floor(generatedLabels[i]);
-            return `${n}+`;
-          } else {
-            const n1 = 1 + Math.floor(generatedLabels[i]);
-            const n2 = Math.floor(generatedLabels[i + 1]);
-            return `${n1} - ${n2}`;
-          }
-        }
-        const legend = legendColor()
-          .shapeWidth(30)
-          .cells(cells)
-          .titleWidth(3)
-          .labels(label)
-          .title("Confirmed Cases")
-          .orient("vertical")
-          .scale(color);
-        svg.select(".legend").call(legend);
+        resetDistrict();
+        setRenderData(kerala);
       })();
     }
-  }, [districts, maxConfirmed, resetDistrict, total.corona_positive, width]);
+  }, [districts, resetDistrict, total.corona_positive]);
 
   useEffect(() => {
     if (width >= 500) {
